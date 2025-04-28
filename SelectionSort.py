52e81ff8577b0009ebc0082013991471d497d11a
Mukesh Kumar <s.m.mukeshkumar1997@gmail.com>
2025-04-28 13:31:46 +0530

    Added Code for Selection Sort

---
+++ SelectionSort.py
@@ -0,0 +1,20 @@
+#Selection Sort
+
+def sort(nums):
+    for i in range(len(nums)):
+        minpos = i
+        for j in range(i+1,len(nums),1):
+            if nums[j] < nums[minpos]:
+                minpos = j
+        temp = nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos] = temp
+
+n = int(input("Enter the length of the list: "))
+mklist = []
+for i in range(n):
+    m = int(input(f"Enter the value {i+1}: "))
+    mklist.append(m)
+
+sort(mklist)
+print(mklist)
